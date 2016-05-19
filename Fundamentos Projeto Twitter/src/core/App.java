de1b763dcdc0c57367a4389c9b7ffe34d55cddfc
Wellington <wellington@wton.com.br>
2016-05-19 12:26:15 -0300

    projeto twitter

--- Fundamentos Projeto Twitter/src/core/App.java
+++ Fundamentos Projeto Twitter/src/core/App.java
@@ -1,5 +1,20 @@
 package core;
 
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class App {
 
+	public Configuration loadConfiguration(){
+		ConfigurationBuilder builder = new ConfigurationBuilder();
+		builder.setOAuthConsumerKey("x");
+		builder.setOAuthConsumerSecret("x");
+		return builder.build();
+	}
+	public AccessToken loadAccessToken(){
+		String token = "1-x";
+		String tokenSecret = "x";
+		return new AccessToken(token, tokenSecret);
+	}
 }
--- Fundamentos Projeto Twitter/src/main/Main.java
+++ Fundamentos Projeto Twitter/src/main/Main.java
@@ -1,9 +1,25 @@
 package main;
 
+import core.App;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			App app = new App();
 
+			TwitterFactory factory = new
+			TwitterFactory(app.loadConfiguration());
+			Twitter twitter = factory.getInstance();
+			twitter.setOAuthAccessToken(app.loadAccessToken());
+			
+			Status status = twitter.updateStatus("Olá Twitter!");
+			System.out.println("Tweet postado com sucesso! [" + status.getText() + "].");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
